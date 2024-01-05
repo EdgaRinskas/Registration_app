dbb254eb9ea23bd49243489b3cbd1331dd9503de
Edgaras Austinskas <eaustinskas@gmail.com>
2024-01-05 15:13:51 +0200

    made front and back connected

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+router.get('/', async (req, res) => {
+  try {
+    const users = await userController.getAllUsers(req, res);
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 router.get('/users', userController.getAllUsers);
 router.post('/users', userController.createUser);
 router.delete('/users/:id', userController.deleteUser);
